d49cb0cd4d113473a4b3b48db0e64cd9d52ab46d
admin <admin@admins-MacBook-Pro.local>
2015-12-07 18:19:36 -0500

    decided to switch to APi as scraping google is against their ToS

--- Homework doer.py
+++ Homework doer.py
@@ -1,14 +1,4 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 
-word = raw_input('Paste your word ')
-
-url = 'https://www.google.com/?gws_rd=ssl#safe=off&q=%s' % word
-
-print url
-response = requests.get(url)
-html = response.content
-print html
-
-soup = BeautifulSoup(html)
-print soup.prettify()
+word = raw_input('Input word')
